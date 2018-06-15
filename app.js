d2b4518f8a122cfcfe190619c4ef7692da2f03bf
Subodh Sharma <subodhss2323@gmail.com>
2018-06-15 15:06:33 -0700

    let's begin

---
+++ app.js
@@ -0,0 +1,33 @@
+let element;
+
+element = document;
+element = document.all;
+element = document.all[200];
+element = document.head;
+element = document.body;
+element	= document.domain;
+element = document.URL;
+element = document.port;
+element = document.charset;
+element = document.contentType;
+
+
+//links
+element = document.links;
+element = document.links[2];
+element = document.links[2].id;
+element = document.links[2].className;
+element = document.links[2].classList;
+
+//forms
+element = document.forms;
+element = document.forms[0];
+element = document.forms[0].id;
+element = document.forms[0].method;
+element = document.forms[0].action;
+element = document.forms[0].classList;
+element = document.forms[0].className;
+
+
+
+console.log(element);
